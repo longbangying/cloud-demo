14db7f2460e0e3e1b69ae5d43f860d02b1b3238b
1523550440@qq.com <1523550440@qq.com>
2019-09-03 16:17:13 +0800

    --feign文件上传 consumer1

---
+++ clouddemo/consumer1/src/main/java/com/example/consumer1/Consumer1Application.java
@@ -0,0 +1,18 @@
+package com.example.consumer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(exclude = RedisAutoConfiguration.class)
+@EnableEurekaClient
+@EnableFeignClients
+public class Consumer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consumer1Application.class, args);
+    }
+
+}
