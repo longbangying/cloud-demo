bd4e223d4d828a6307cdaa5e7f60de1679cdee40
1523550440@qq.com <1523550440@qq.com>
2019-12-05 18:16:48 +0800

    --网关

--- clouddemo/oauth2-memory/src/main/java/com/xbang/oauth2/config/AuthConfig.java
+++ clouddemo/oauth2-memory/src/main/java/com/xbang/oauth2/config/AuthConfig.java
@@ -1,12 +1,12 @@
 package com.xbang.oauth2.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -17,10 +17,19 @@ public class AuthConfig extends AuthorizationServerConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        //在内存中构建一个clientID
         clients.inMemory()
-                .withClient("xbang").redirectUris("http://www.baidu.com").scopes("all").authorizedGrantTypes("authorization_code").secret(bCryptPasswordEncoder().encode("xbang"));
+                .withClient("xbang").redirectUris("http://www.baidu.com").scopes("all").authorizedGrantTypes("authorization_code","refresh_token").secret(bCryptPasswordEncoder().encode("xbang"));
+
+        //clients.withClientDetails(clientDetailsService);
     }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        //配置 /oauth/check_token 免登录
+        security.checkTokenAccess("permitAll()");
+    }
+
 }
