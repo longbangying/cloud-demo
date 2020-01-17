70fb52040840e724ff6f891d4cc80ac29c4539ba
1523550440@qq.com <1523550440@qq.com>
2020-01-17 17:25:30 +0800

    --cloud-config,cloud-bus-kafka

--- clouddemo/spring-cloud-config-demo/spring-cloud-config-client/src/main/java/com/example/config/client/ConfigClientApplication.java
+++ clouddemo/spring-cloud-config-demo/spring-cloud-config-client/src/main/java/com/example/config/client/ConfigClientApplication.java
@@ -1,14 +1,39 @@
 package com.example.config.client;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @SpringBootApplication
+@RefreshScope
+@RestController
 public class ConfigClientApplication {
+
+
+    @Value("${xbang.name:xbangss}")
+    private String xbangName;
+
+    @Value("${xbang.passwd}")
+    private String passwd;
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class,args);
+    }
+
+    @PostMapping("info")
+    public Map info(){
+        Map resultMap = new HashMap();
+        resultMap.put("name",xbangName);
+        resultMap.put("passwd",passwd);
+        return resultMap;
     }
 }
--- clouddemo/spring-cloud-config-demo/spring-cloud-config-server/src/main/java/com/example/config/server/ConfigServerApplication.java
+++ clouddemo/spring-cloud-config-demo/spring-cloud-config-server/src/main/java/com/example/config/server/ConfigServerApplication.java
@@ -3,10 +3,12 @@ package com.example.config.server;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
